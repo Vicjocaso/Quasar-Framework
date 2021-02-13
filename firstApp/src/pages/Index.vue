e9bb6e85483a72cadace6b556415afcbbc86f0de
Vctor José Calderón <54919154+Vicjocaso@users.noreply.github.com>
2021-02-13 10:40:43 -0400

    quasar

--- firstApp/src/pages/Index.vue
+++ firstApp/src/pages/Index.vue
@@ -1,14 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <input v-model="message"
+    @keyup.esc="clearMessage"
+    @keyup.enter="alertMessage">
+    <button @click="clearMessage()">Click</button>
+    <h1 class="border-grey" v-show="message.length"> {{message}} </h1>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data () {
+    return {
+      message: 'Quasar is fun '
+    }
+  },
+  methods: {
+    clearMessage () {
+      this.message = ''
+    },
+    alertMessage () {
+      alert(this.message)
+    }
+  }
 }
 </script>
+<style>
+ .border-grey {
+   border: 1px solid grey;
+ }
+
+</style>
