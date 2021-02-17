07c3785a0588ef36f50b62f445935ca786e9a71a
Vctor José Calderón <54919154+Vicjocaso@users.noreply.github.com>
2021-02-16 21:26:52 -0400

    child Components

---
+++ firstApp/src/components/Task.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>
+    <div><slot></slot>{{ index + 1 }}</div>
+    <small>{{ task.date }} : {{ task.time }}</small>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['task', 'index']
+}
+</script>
+
+<style></style>
--- firstApp/src/pages/Index.vue
+++ firstApp/src/pages/Index.vue
@@ -1,15 +1,9 @@
 <template>
   <q-page padding>
     <ul>
-      <li v-for="(task,index) in task" :key="task">
-        <div>
-          {{task.name}} {{ index + 1  }}
-        </div>
-        <small>{{task.date}} {{task.time}}</small>
-        <button @click="deleteTask(index)">X</button>
-      </li>
+      <task v-for="(task, index) in task" :key="task.id" :task="task" :index="index">{{task.name}}</task>
     </ul>
-      </q-page>
+  </q-page>
 </template>
 
 <script>
@@ -18,16 +12,19 @@ export default {
     return {
       task: [
         {
+          id: '1',
           name: 'Go to shop',
           date: '2020/02/15',
           time: '8:00'
         },
         {
+          id: '2',
           name: 'Get banana',
           date: '2019/03/28',
           time: '3:00'
         },
         {
+          id: '3',
           name: 'Feed Rocky',
           date: '2022/04/25',
           time: '7:00'
@@ -35,12 +32,12 @@ export default {
       ]
     }
   },
-  methods: {
-    deleteTask (index) {
-      this.task.splice(index, 1)
-    }
+
+  components: {
+    task: require('components/Task.vue').default
   }
 }
 </script>
 <style>
+
 </style>
